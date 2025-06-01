75d46e9cb981883137a20358c9f17dd68220971d
kakluo <cmweifan@gmail.com>
2025-06-01 22:08:18 +0800

    Update Editor.js

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -12,11 +12,11 @@ import { THEMES } from "../utils/constants";
 const defaultIcon = { 'label': 'react', 'value': 'react' }
 
 const defaultSettings = {
-	title: "清羽飞扬的文章标题",
+	title: "东方月初的文章标题",
 	bgColor: "#949ee5",
 	pattern: "",
 	download: "PNG",
-	author: "LiuShen's Blog",
+	author: "BiuXin's Blog",
 	icon: defaultIcon,
 	devIconOptions: [defaultIcon],
 	font: 'font-Anek',
